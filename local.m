5ef13c90c5ed6f960af51a7c4effedcafc508be7
chemgeeklian <xinran.lian@gmail.com>
2020-11-11 17:59:59 -0600

    bug fixed

--- local.m
+++ local.m
@@ -1,4 +1,4 @@
-lib=fastaread('Inputs/localkeep_New_Proteins.fasta');
+lib=fastaread('Outputs/localkeep_New_Proteins.fasta');
 localgene = load('Inputs/local_gene.mat').gene;
 hits=blastreadlocal('Inputs/blast_local2.txt', 8);
 
@@ -65,57 +65,3 @@ end
 lib1=lib;
 save('Outputs/lib_local.mat','lib')
 
-sc_codon_usage = load('s_cerevisiae_codon_usage.mat').sc_codon_usage;
-codon_freq = sc_codon_usage.codon_frequencies;
-aa=sc_codon_usage.aa;
-codon_table = sc_codon_usage.aa_codon;
-resites={'GGATCC','GAATTC','AGCGCT','GCAGTG','CACTGC','AAAAA','GGGGG','CCCC C','TTTTT'}; 
-
-%for i=1:numel(lib) 
-%    lib(i).oligo_assemble=resite_remove(lib(i).dna,resites); 
-%    if mod(i,1000)==0
-%        disp(num2str(i));
-%    end
-%end
-
-function seq=resite_remove(dna, resites)
-% SEQ = RESITE_remove(DNA, RESITES) accepts a DNA sequence that is in
-% frame, and finds restriction sites found in the cell array RESITES. If
-% none are found, the dna sequence is returned unchanged to the variable
-% SEQ. If RESITES are found, then synonymous substitutions are made to dna % to remove RESITES, and this edited dna sequence is returned to SEQ
-% subramaniansk@gmail.com
-% codons are picked from yeast codon usage. 
-sc_codon_usage=load('s_cerevisiae_codon_usage.mat').sc_codon_usage;
-codon_freq = sc_codon_usage.codon_frequencies;
-aa=sc_codon_usage.aa;
-codon_table = sc_codon_usage.aa_codon;
-seq=upper(dna);
-resites=upper(resites);
-pro=nt2aa(seq,'AlternativeStartCodons','False');
-re_flag =0;
-while re_flag < 1
-for i=1:numel(resites) loc=strfind(seq, resites{i}); if numel(loc)>=1
-amino acid
-for j=1:numel(loc)
-pos=ceil(loc(j)/3);
-% pick pos or pos+1 as the codon to change if (pos < numel(pro)-1) && (rand()>0.6)
-pos=pos+1;
-end
-cod=codon_table{aa==pro(pos)};% get all codons for the
-% pick a codon randomly, based on its frequency.
-cod_freq=codon_freq(aa==pro(pos),:); cod_freq(cod_freq==0)=[];
-cod_freq=cumsum(cod_freq);cod_freq=cod_freq/cod_freq(end); pick_cod=cod{find(rand() < cod_freq,1,'first')};
-% switch in the new codon
-seq(pos*3-2:pos*3)=pick_cod;
-end
-end
-end
-% we've gone through the sequence once and swiched codons where ever % we saw RE sites. this might not have removed all RE sites because % (a) the random switch might put back the WT codon i.e. create no
-% change
-% (b) the switch might itself create a new RE site.
-% So, check for the presense of re sites again and set re_flag. re_flag =10;
-for i=1:numel(resites)
-if strfind(seq,resites{i}) re_flag=0;
-end
-end
-end
